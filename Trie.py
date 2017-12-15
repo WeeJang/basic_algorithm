5d22b4d7ad65c86e2493900339a73c7636db4e0d
jiangwei <jiangwei@dingfudata.com>
2017-12-15 14:33:40 +0800

    dev

--- Trie.py
+++ Trie.py
@@ -1,12 +1,15 @@
 #!/user/bin/env python2
 #-*- coding:utf-8 -*-
 
+"""
+http://blog.csdn.net/wwh578867817/article/details/44886709
+"""
 
 
 class Trie(object):
-	""" 后缀树 """
+	""" 前缀树／字典树／Trie树 """
 	
-	def __init__(self,str_set):
+	def __init__(self,string):
 		self.trie = {}
 		self.init_trie(str_set)
 
