82f0d7b481507c24e4da78ee4c756481e276ba8a
jiangwei <jiangwei@dingfudata.com>
2018-01-10 17:04:02 +0800

    dev

--- SubsetII.py
+++ SubsetII.py
@@ -39,74 +39,53 @@ len(R) = 2^N
 
 有没有发现，当第i个元素加入时，产生的新子集其实是组合了前面所有子集产生的。
 因此可以迭代完成.
+
+
+
+但是如果有重复元素呢？
+
+我确实没想到，但看到leetcode上的分析，精妙！https://leetcode.com/problems/subsets-ii/discuss/30168
+比如 例子中 [1,2,3] 其实当出现一个新元素3,新的子集相当于加这个元素产生的集合.
+     也就是说全集是[1,2]的子集S1 + S1与3组合的子集S2
+     也就是说全集是[1,2]'不添加3' 和 '添加3'两个状态后的子集和
+对于 [1,2,3,3] 可以把(3,3) 当成一个special元素,
+     那全集就是[1,2] '添加0个3','添加1个3','添加2个3' 这3个状态后的子集和
+
+所以一上来得先排个序，比如说(1,2,2,3,3,..),每个元素对应的个数为k1,k2,k3  所以产生的子集个数为
+(k1+1)*(k2+1)*(k3+1)
+
 """
 
 import copy
 class Solution(object):
     
-	def subsetsWithNoDup(self, nums):
+	def subsetsWithDup(self, nums):
 		result = []
 		if nums is None or len(nums) == 0:
 			return result
+		nums = sorted(nums)
+		nums.append("dummy") #dummy
 		result.append([])
-		print result
+		last_value = None
+		counter = 0
 		for e in nums:
-			new_subset = copy.deepcopy(result)		
-			for set_ in new_subset:
-				set_.append(e)
-			result.extend(new_subset)
+			if last_value is None:
+				last_value = e
+				counter = 1
+				continue
+			elif last_value is not None and last_value == e:
+				counter += 1
+				continue
+			new_subset = copy.deepcopy(result)
+			for i in range(counter):
+				new_subset_copy = copy.deepcopy(new_subset)	
+				for set_ in new_subset_copy:
+					set_.extend([last_value]*(i+1))
+				result.extend(new_subset_copy)
+			last_value = e
+			counter = 1
 		return result
 
-nums = [1,2,3]
+nums = [1,2,2,3]
 solver = Solution()
-print solver.subsetsWithNoDup(nums)
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print solver.subsetsWithDup(nums)
